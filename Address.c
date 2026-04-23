68090e06d3a2a695bbe439084b7c3c184c276730
Yashec21 <yashpatilec21@gmail.com>
2026-04-23 23:15:23 +0530

    Add files via upload

---
+++ Address.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch ='A';
+    int i = 11;
+    float f=99.99f;
+    double d=78.67345;
+
+    printf("L values of all variable\n");
+
+    printf("%u\n",&ch);
+    printf("%u\n",&i);
+    printf("%u\n",&f);
+    printf("%u\n",&d);
+
+
+    return 0;
+}
